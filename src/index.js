ea8a83c26094178c3e5bb9ec16e8f514536ad2e6
Stuart Lee <stuart.lee@drpgroup.com>
2018-08-15 22:42:41 +0100

    load from map

--- src/index.js
+++ src/index.js
@@ -3,24 +3,44 @@ const ctx = canvas.getContext('2d')
 
 const WIDTH = 320
 const HEIGHT = 240
+const SIZE = 8
 
 canvas.width = WIDTH
 canvas.height = HEIGHT
 
 ctx.fillStyle = '#000'
-ctx.fillRect(0,0, WIDTH, HEIGHT)
+ctx.fillRect(0, 0, WIDTH, HEIGHT)
+
+const map = [
+  '1111111111111111',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1              1',
+  '1111111111111111',
+]
 
 let sprites = new Image()
-sprites.onload = () => {
-  drawTile(0, 0)
-  drawTile(0, 8)
-  drawTile(0, 16)
-  drawTile(0, 24)
-  drawTile(0, 32)
-}
+sprites.onload = drawMap
 sprites.src = './sprites.png'
 
+function drawMap () {
+  for (let y = 0; y < map.length; y++) {
+    const row = map[y].split('')
+    for (let x = 0; x < row.length; x++) {
+      const tile = map[y][x]
+      if (tile === ' ') continue
+      drawTile(x * SIZE, y * SIZE)
+    }
+  }
+}
 
 function drawTile (x, y) {
-  ctx.drawImage(sprites, 0, 0, 8, 8, x, y, 8, 8)
+  ctx.drawImage(sprites, 0, 0, SIZE, SIZE, x, y, SIZE, SIZE)
 }
