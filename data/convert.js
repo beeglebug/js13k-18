f229b20e1842aa8ee1a061737be9f2e06794aaef
Stuart Lee <stuart.lee@drpgroup.com>
2018-08-29 20:29:31 +0100

    import objects

--- data/convert.js
+++ data/convert.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const glob = require('glob')
 
+const TILE_SIZE = 8
 const rooms = []
 
 glob('data/*.json', (_, files) => {
@@ -17,12 +18,23 @@ function parseMap (map) {
     if (layer.type === 'tilelayer') {
       room.data = layer.data.join('')
     }
+    if (layer.type === 'objectgroup') {
+      room.items = layer.objects.map(parseObject)
+    }
   })
   rooms.push(room)
 }
+
+function parseObject (object) {
+  const { type, x, y } = object
+  const props = [type, x / TILE_SIZE, (y / TILE_SIZE) - 1]
+  if (type === 'S') props.push(object.properties.text)
+  return props.join('|')
+}
+
+// 'S|7|3|An ancient carving\nYou can\'t decipher it'
 
 function writeOutput () {
   const output = 'let rooms = ' + JSON.stringify(rooms)
   fs.writeFileSync('./src/js/rooms.js', output)
 }
-
